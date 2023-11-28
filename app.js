edded866aa1a9a407fbcec875e2f00edaaa9d4d0
JosephGray514 <josephgray514@gmail.com>
2023-11-28 16:31:16 -0600

    Primer commit de Sign In

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ app.post('/login', (req, res) => {
     res.send('ruta de login nueva')
 })
 
+app.post('/signin', (req, res) => {
+    res.send('ruta de sign in nueva')
+})
+
 app.listen(3000, (req, res) => {
     console.log('Listening')
 })
