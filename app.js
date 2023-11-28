aa7c6078376469011abdace2f6856566f888e887
JosephGray514 <josephgray514@gmail.com>
2023-11-28 15:48:25 -0600

    Primer commit de login

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.post('/login', (req, res) => {
+    res.send('ruta de login nueva')
+})
+
 app.listen(3000, (req, res) => {
     console.log('Listening')
 })
